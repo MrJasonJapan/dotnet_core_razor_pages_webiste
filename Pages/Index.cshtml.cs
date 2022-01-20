2ee96de8aa0c37f9289b7c89ab66b131f6527893
SpaGettys <jgettys@gmail.com>
2022-01-20 20:31:58 +0900

    Initial Commit
    ASP.NET Core CRUD App w/ Razor Pages (.NET 6)

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace dotnet_core_razor_pages_webiste.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+
+    }
+}
